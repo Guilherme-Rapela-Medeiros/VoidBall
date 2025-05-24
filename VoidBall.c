b771b2df38ff5a0f9c0a3f838de10e1cd94d00b5
José Vitor Veloso <114881732+vitor-veloso@users.noreply.github.com>
2025-05-24 01:49:55 -0300

    Inclusão de "includes" e definição de valores

---
+++ VoidBall.c
@@ -0,0 +1,18 @@
+#include "raylib.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define LARGURA_TELA 800
+#define ALTURA_TELA 600
+#define VELOCIDADE_JOGADOR 8
+#define RAIO_BOLA 10
+#define LARGURA_INIMIGO 80
+#define ALTURA_INIMIGO 200
+#define MAXIMO_RAIOS_TELA_JOGADOR 5
+#define MAXIMO_RAIOS_TELA_INIMIGO 5
+#define MAXIMO_LEN_NOMES 50
+#define MAXIMO_SCORES_TELA 10
+
+
+
