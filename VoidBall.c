2246be7b350ac7d9a62eac5f6f39d6838938c118
José Vitor Veloso <114881732+vitor-veloso@users.noreply.github.com>
2025-05-24 02:39:42 -0300

    Inclusão de Structs

--- VoidBall.c
+++ VoidBall.c
@@ -3,16 +3,29 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define LARGURA_TELA 800
-#define ALTURA_TELA 600
-#define VELOCIDADE_JOGADOR 8
+#define LARGURA_TELA 900
+#define ALTURA_TELA 700
+#define VELOCIDADE_PLAYER 8
 #define RAIO_BOLA 10
 #define LARGURA_INIMIGO 80
 #define ALTURA_INIMIGO 200
-#define MAXIMO_RAIOS_TELA_JOGADOR 5
+#define MAXIMO_RAIOS_TELA_PLAYER 5
 #define MAXIMO_RAIOS_TELA_INIMIGO 5
-#define MAXIMO_LEN_NOMES 50
+#define LEN_NOME 4
 #define MAXIMO_SCORES_TELA 10
 
+typedef struct{
+    Vector2 posicao;
+    Vector2 velocidade;
+}Bola;
 
+typedef struct{
+    Vector2 posicao;
+    int ativo;
+}Raio;
 
+typedef struct Pontuacao_Player {
+    int score;
+    char nome[LEN_NOME];
+    struct Pontuacao_Player *prox
+}Pontuacao_Player;
