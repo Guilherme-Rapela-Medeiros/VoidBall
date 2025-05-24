4c4fe73c7a0d716d205f8508b8fe591be1e12b4f
José Vitor Veloso <114881732+vitor-veloso@users.noreply.github.com>
2025-05-24 14:38:57 -0300

    criada função de add pontuação

--- VoidBall.c
+++ VoidBall.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-#define LARGURA_TELA 900
-#define ALTURA_TELA 700
+
 #define VELOCIDADE_PLAYER 8
 #define RAIO_BOLA 10
 #define LARGURA_INIMIGO 80
-#define ALTURA_INIMIGO 200
+#define ALTURA_INIMIGO 20
 #define MAXIMO_RAIOS_TELA_PLAYER 5
 #define MAXIMO_RAIOS_TELA_INIMIGO 5
 #define LEN_NOME 4
@@ -25,7 +25,39 @@ typedef struct{
 }Raio;
 
 typedef struct Pontuacao_Player {
-    int score;
+    int pontuacao;
     char nome[LEN_NOME];
-    struct Pontuacao_Player *prox
+    struct Pontuacao_Player *prox;
 }Pontuacao_Player;
+
+Pontuacao_Player *Adicionar_pontuacao(Pontuacao_Player *head, const char *nome, int pontuacao){
+    Pontuacao_Player *novo_no = (Pontuacao_Player*)malloc(sizeof(Pontuacao_Player));
+    if(novo_no == NULL){
+        return head;
+    }
+    strcpy(novo_no->nome, nome);
+    novo_no->pontuacao = pontuacao;
+    novo_no->prox = NULL;
+
+    if(head == NULL){
+        return novo_no;
+    }
+
+    Pontuacao_Player *atual = head;
+    while(atual->prox != NULL){
+        atual = atual->prox;
+    }
+    atual->prox = novo_no;
+    return head;
+}
+
+int main(){
+    int largura_tela = 900;
+    int altura_tela = 700;
+
+    InitWindow(largura_tela, altura_tela, "VoidBall");
+    SetTargetFPS(60);
+
+    Rectangle player = { largura_tela / 2, altura_tela - 20, 100, 20 };
+    
+}
